a21c1c18aa11b7e2dce6a124e22466118a096164
randoi <72024095+randoi@users.noreply.github.com>
2023-03-07 02:22:14 +0100

    Add files via upload
    Dodavanje mikroservisa za zaposlene.

---
+++ employees/src/test/java/raf/bolnica1/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.bolnica1.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
