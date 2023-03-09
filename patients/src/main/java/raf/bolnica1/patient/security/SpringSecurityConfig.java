3526760a58d4c2c7b3cd8cf8e6615be39c6714c9
randoi <randoimarkovic@gmail.com>
2023-03-09 13:02:17 +0100

    Dodat kostur za mikroservis pacijenata

---
+++ patients/src/main/java/raf/bolnica1/patient/security/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package raf.bolnica1.patient.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    //Ovde deklarisati filtere i auth service koji nasledjuje user details service
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .antMatchers("/h2-console/").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/")
+                .and().headers().frameOptions().sameOrigin();
+
+                 http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        //Ovde ide dodavanje filtera za autorizaciju i jwt proveru tokena
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
