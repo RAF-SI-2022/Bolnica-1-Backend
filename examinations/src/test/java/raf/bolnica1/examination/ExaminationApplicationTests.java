32a5c0accb7d3bc0cd0bdf8b88064984d26158df
KYGAS <acazvezdas@gmail.com>
2023-03-11 19:23:10 +0100

    Dodat kostur za ExaminationApplication : - Dodat pom sa dependenci-ima - Dodat spring security - Dodat spring

---
+++ examinations/src/test/java/raf/bolnica1/examination/ExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.bolnica1.examination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
