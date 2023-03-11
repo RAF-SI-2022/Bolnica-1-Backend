c7965b85b7720365989e6ccce200621ae5821352
petar-mitic <mitic22@Petars-MacBook-Air.local>
2023-03-11 18:46:13 +0100

    Implemenitrani MedicalRecordDto, MedicalRecordMapper i f-ja findMedicalRecordByLbp

--- patients/src/main/java/raf/bolnica1/patient/controllers/PatientController.java
+++ patients/src/main/java/raf/bolnica1/patient/controllers/PatientController.java
@@ -1,21 +1,18 @@
 package raf.bolnica1.patient.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import raf.bolnica1.patient.dto.MedicalRecordDto;
 import raf.bolnica1.patient.dto.PatientDto;
 import raf.bolnica1.patient.services.PatientService;
 
 import java.util.List;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import raf.bolnica1.patient.services.PatientService;
 
+import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 
 
@@ -107,9 +104,14 @@ public class PatientController {
 
 
     //Svi kartoni
-    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "/findMedicalChart/{ppn}")
-    public ResponseEntity<Object> findMedicalChartPatient(@PathVariable("ppn") Long ppn, @Valid @RequestBody Object object){
-        return null;
+    @GetMapping(
+            path = "/findMedicalRecord/{ppn}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<MedicalRecordDto>> findMedicalRecordByLbp(@PathVariable("ppn") String lbp){
+
+        //provera jwt tokena zbog privilegija
+
+        return ResponseEntity.ok(patientService.findMedicalRecordByLbp(lbp));
     }
 
 
--- patients/src/main/java/raf/bolnica1/patient/repository/MedicalRecordRepository.java
+++ patients/src/main/java/raf/bolnica1/patient/repository/MedicalRecordRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import raf.bolnica1.patient.domain.MedicalRecord;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+
+    Optional<List<MedicalRecord>> findByPatientLbp(String lbp);
 }
