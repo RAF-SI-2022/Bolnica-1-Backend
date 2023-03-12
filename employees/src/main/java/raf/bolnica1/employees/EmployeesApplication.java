e1b672d19f3dfe0616ab37e23fecb92a05145b7c
jnajdic3919rn <jnajdic3919rn@raf.rs>
2023-03-12 15:46:43 +0100

    Obrisani komentari

--- employees/src/main/java/raf/bolnica1/employees/EmployeesApplication.java
+++ employees/src/main/java/raf/bolnica1/employees/EmployeesApplication.java
@@ -13,18 +13,5 @@ public class EmployeesApplication {
         SpringApplication.run(EmployeesApplication.class, args);
     }
 
-    /**
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
-    */
-
 
 }
