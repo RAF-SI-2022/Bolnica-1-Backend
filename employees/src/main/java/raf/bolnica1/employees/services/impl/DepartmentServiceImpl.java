741abf74943877b19095de441a0f66c86f8c3f30
jnajdic3919rn <jnajdic3919rn@raf.rs>
2023-03-13 14:01:07 +0100

    Promene u beku za paginaciju.

--- employees/src/main/java/raf/bolnica1/employees/services/impl/DepartmentServiceImpl.java
+++ employees/src/main/java/raf/bolnica1/employees/services/impl/DepartmentServiceImpl.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import raf.bolnica1.employees.domain.Department;
+import raf.bolnica1.employees.domain.Hospital;
 import raf.bolnica1.employees.dto.department.DepartmentDto;
+import raf.bolnica1.employees.dto.department.HospitalDto;
 import raf.bolnica1.employees.repository.DepartmentRepository;
+import raf.bolnica1.employees.repository.HospitalRepository;
 import raf.bolnica1.employees.services.DepartmentService;
 
 import java.util.ArrayList;
@@ -17,6 +20,7 @@ import java.util.List;
 public class DepartmentServiceImpl implements DepartmentService {
 
     private DepartmentRepository departmentRepository;
+    private HospitalRepository hospitalRepository;
 
     @Override
     public List<DepartmentDto> listAllDepartments() {
@@ -25,5 +29,14 @@ public class DepartmentServiceImpl implements DepartmentService {
             departmentDtos.add(new DepartmentDto(department.getId(), department.getPbo(), department.getName(), department.getHospital().getShortName()));
         }
         return departmentDtos;
+    }
+
+    @Override
+    public List<HospitalDto> listAllHospitals() {
+        List<HospitalDto> hospitalDtos = new ArrayList<>();
+        for(Hospital hospital : hospitalRepository.findAll()){
+            hospitalDtos.add(new HospitalDto(hospital.getId(), hospital.getShortName()));
+        }
+        return hospitalDtos;
     }
 }
