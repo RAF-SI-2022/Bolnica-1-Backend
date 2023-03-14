f499b434cac236e4a860ce41514a44006cb96b6f
fcolic89 <filip.colic00@gmail.com>
2023-03-14 11:09:44 +0100

    Dodata klasa za integracione testove

--- patients/src/test/java/raf/bolnica1/patient/PatientServiceIntegrationTest.java
+++ patients/src/test/java/raf/bolnica1/patient/PatientServiceIntegrationTest.java
@@ -1,2 +1,25 @@
-package raf.bolnica1.patient;public class PatientServiceIntegrationTest {
+package raf.bolnica1.patient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PatientServiceIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    @Test
+    void registerPatient(){
+        
+    }
+
 }
