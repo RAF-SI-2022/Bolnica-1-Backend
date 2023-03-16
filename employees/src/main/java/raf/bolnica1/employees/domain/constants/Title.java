c53672b98f9cf6cfde72d9b4fe75f344c91dc069
Ognjen Prica <basedev0@gmail.com>
2023-03-16 17:55:33 +0100

    phase2

--- employees/src/main/java/raf/bolnica1/employees/domain/constants/Title.java
+++
@@ -1,9 +0,0 @@
-package raf.bolnica1.employees.domain.constants;
-
-public enum Title {
-    PROF_DR_MED,
-    DR_MED_SPEC,
-    DR_SCI_ME,
-    DIPL_FARM,
-    MAG_FARM, MR;
-}
--- employees/src/main/java/raf/bolnica1/employees/services/EmployeeRoleService.java
+++
@@ -1,9 +0,0 @@
-package raf.bolnica1.employees.services;
-
-import raf.bolnica1.employees.dto.role.RoleDto;
-
-import java.util.List;
-
-public interface EmployeeRoleService {
-    List<RoleDto> privilegeForEmployee(String lbz);
-}
