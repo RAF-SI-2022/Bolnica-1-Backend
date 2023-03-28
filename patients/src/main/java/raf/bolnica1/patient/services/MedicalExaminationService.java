91a735d35a034564ca64a57cdf39310b10be9372
randoi <randoimarkovic@gmail.com>
2023-03-28 14:20:43 +0200

    Prebacene metode

--- patients/src/main/java/raf/bolnica1/patient/services/MedicalExaminationService.java
+++ patients/src/main/java/raf/bolnica1/patient/services/MedicalExaminationService.java
@@ -10,4 +10,18 @@ public interface MedicalExaminationService {
     ExaminationHistoryDto addExamination(String lbp, ExaminationHistoryCreateDto examinationHistoryCreateDto);
 
     MedicalHistoryDto addMedicalHistory(String lbp, MedicalHistoryCreateDto medicalHistoryCreateDto);
+
+    //Pretraga zakazanih pregleda
+    public Object findScheduledExamination(Object object);
+    // Azuriranje statusa o prispecu pacijenta
+    public Object updatePatientArrivalStatus(Object object);
+
+    // Azuriranje statusa pregleda
+    public Object updateExaminationStatus(Object object);
+
+    //Brisanje zakazanog pregleda
+    public Object deleteScheduledExamination(Object object);
+
+    // Pretraga lekara specijalista na odeljenju
+    public Object findDoctorSpecByDepartment(Object object);
 }
