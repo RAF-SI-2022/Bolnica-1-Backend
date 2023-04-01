c9964107ebac90f681d712084f3ff4b317f2449d
fcolic <fcolic8920rn@raf.rs>
2023-04-01 14:51:28 +0200

    Dodato kreiranje zakazanog termina prijema.

--- infirmary/src/main/java/raf/bolnica1/infirmary/controllers/AppointmentController.java
+++ infirmary/src/main/java/raf/bolnica1/infirmary/controllers/AppointmentController.java
@@ -1,9 +1,23 @@
 package raf.bolnica1.infirmary.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import raf.bolnica1.infirmary.dto.ScheduleAppointmentDto;
+import raf.bolnica1.infirmary.services.AppointmentService;
 
 @RestController
 @RequestMapping("/appointment")
+@AllArgsConstructor
 public class AppointmentController {
+
+    AppointmentService appointmentService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createAppointment(@RequestHeader("Authorization") String authorization, @RequestBody ScheduleAppointmentDto appointmentDto){
+        return new ResponseEntity<>(appointmentService.createAppointment(authorization, appointmentDto), HttpStatus.OK);
+    }
+
 }
