74cb361838dcbd0392af4d87e028a914813e397b
fcolic <fcolic8920rn@raf.rs>
2023-04-01 14:48:35 +0200

    Izmena u ScheduleAppointmentDto.

--- infirmary/src/main/java/raf/bolnica1/infirmary/dto/ScheduleAppointmentDto.java
+++ infirmary/src/main/java/raf/bolnica1/infirmary/dto/ScheduleAppointmentDto.java
@@ -1,14 +1,10 @@
 package raf.bolnica1.infirmary.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class ScheduleAppointmentDto {
