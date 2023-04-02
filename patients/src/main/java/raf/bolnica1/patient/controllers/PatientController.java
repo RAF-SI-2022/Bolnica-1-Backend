c9a534a2edeee4d5bd9ad5da63ab89520fe38f3a
randoi <randoimarkovic@gmail.com>
2023-04-03 00:10:36 +0200

    Nista

--- patients/src/main/java/raf/bolnica1/patient/controllers/PatientController.java
+++ patients/src/main/java/raf/bolnica1/patient/controllers/PatientController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
