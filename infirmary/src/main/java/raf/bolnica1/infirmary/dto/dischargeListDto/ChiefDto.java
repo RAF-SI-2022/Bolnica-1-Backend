fb68d5ee3031947dd9c2566add5fcd14bf0f2178
petar-mitic <mitic22@Petars-MacBook-Air.local>
2023-04-02 10:28:41 +0200

    Implementirana metoda findDischargeListHistory

---
+++ infirmary/src/main/java/raf/bolnica1/infirmary/dto/dischargeListDto/ChiefDto.java
@@ -0,0 +1,17 @@
+package raf.bolnica1.infirmary.dto.dischargeListDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ChiefDto {
+
+    private String name;
+    private String surname;
+    private String profession;
+}
---
+++ infirmary/src/main/java/raf/bolnica1/infirmary/dto/dischargeListDto/PrescriptionDto.java
@@ -0,0 +1,15 @@
+package raf.bolnica1.infirmary.dto.dischargeListDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PrescriptionDto {
+
+    private String referralDiagnosis;
+}
