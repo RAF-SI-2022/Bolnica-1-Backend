c1f50d182df7bef4f78890b7785e0e760e3b7000
stefanbalaz2 <stefanbalaz2@gmail.com>
2023-04-03 17:53:58 +0200

    Dodato dobavljanje Parameter po Analysis ID

--- laboratory/src/main/java/raf/bolnica1/laboratory/controllers/LaboratoryAnalysisParameterController.java
+++ laboratory/src/main/java/raf/bolnica1/laboratory/controllers/LaboratoryAnalysisParameterController.java
@@ -1,12 +1,14 @@
 package raf.bolnica1.laboratory.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import raf.bolnica1.laboratory.dto.lab.analysisParameter.AnalysisParameterDto;
 import raf.bolnica1.laboratory.dto.lab.labAnalysis.LabAnalysisDto;
+import raf.bolnica1.laboratory.dto.lab.parameter.ParameterDto;
 import raf.bolnica1.laboratory.dto.response.MessageDto;
 import raf.bolnica1.laboratory.services.lab.AnalysisParameterService;
 
@@ -42,5 +44,13 @@ public class LaboratoryAnalysisParameterController {
         return new ResponseEntity<>(analysisParameterService.getAnalysisParameter(id), HttpStatus.OK);
     }
 
+
+    @GetMapping("/getParametersByAnalysisId")
+    @PreAuthorize("hasAnyRole('ROLE_LAB_TEHNICAR','ROLE_VISI_LAB_TEHNICAR')")
+    public ResponseEntity<Page<ParameterDto>> getParameterByAnalysisId(@RequestParam("id") Long id,
+                                                                       @RequestParam(defaultValue = "0") Integer page,
+                                                                       @RequestParam(defaultValue = "2") Integer size) {
+        return new ResponseEntity<>(analysisParameterService.getParametersByAnalysisId(id,page,size), HttpStatus.OK);
+    }
 
 }
--- laboratory/src/main/java/raf/bolnica1/laboratory/services/lab/impl/AnalysisParameterServiceImpl.java
+++ laboratory/src/main/java/raf/bolnica1/laboratory/services/lab/impl/AnalysisParameterServiceImpl.java
@@ -1,11 +1,17 @@
 package raf.bolnica1.laboratory.services.lab.impl;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import raf.bolnica1.laboratory.domain.lab.AnalysisParameter;
+import raf.bolnica1.laboratory.domain.lab.Parameter;
 import raf.bolnica1.laboratory.dto.lab.analysisParameter.AnalysisParameterDto;
+import raf.bolnica1.laboratory.dto.lab.parameter.ParameterDto;
 import raf.bolnica1.laboratory.dto.response.MessageDto;
 import raf.bolnica1.laboratory.mappers.AnalysisParameterMapper;
+import raf.bolnica1.laboratory.mappers.ParameterMapper;
 import raf.bolnica1.laboratory.repository.AnalysisParameterRepository;
 import raf.bolnica1.laboratory.services.lab.AnalysisParameterService;
 
@@ -16,6 +22,7 @@ public class AnalysisParameterServiceImpl implements AnalysisParameterService {
 
     private final AnalysisParameterRepository analysisParameterRepository;
     private final AnalysisParameterMapper analysisParameterMapper;
+    private final ParameterMapper parameterMapper;
 
     @Override
     public AnalysisParameterDto createAnalysisParameter(AnalysisParameterDto analysisParameterDto) {
@@ -50,5 +57,12 @@ public class AnalysisParameterServiceImpl implements AnalysisParameterService {
     @Override
     public AnalysisParameterDto getAnalysisParameter(Long id) {
         return analysisParameterMapper.toDto(analysisParameterRepository.findAnalysisParameterById(id));
+    }
+
+    @Override
+    public Page<ParameterDto> getParametersByAnalysisId(Long id,int page,int size) {
+        Pageable pageable= PageRequest.of(page,size);
+        Page<Parameter> parameters=analysisParameterRepository.findParametersByAnalysisId(pageable,id);
+        return parameters.map(parameterMapper::toDto);
     }
 }
