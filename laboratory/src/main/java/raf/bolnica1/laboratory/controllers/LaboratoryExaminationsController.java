c0465c43aa3f3b0f62148cd3d25bb6364854f4b2
randoi <72024095+randoi@users.noreply.github.com>
2023-04-20 15:39:06 +0200

    Update LaboratoryExaminationsController.java

--- laboratory/src/main/java/raf/bolnica1/laboratory/controllers/LaboratoryExaminationsController.java
+++ laboratory/src/main/java/raf/bolnica1/laboratory/controllers/LaboratoryExaminationsController.java
@@ -42,8 +42,8 @@ public class LaboratoryExaminationsController {
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @PreAuthorize("hasAnyRole('ROLE_LAB_TEHNICAR','ROLE_VISI_LAB_TEHNICAR')")
-    public ResponseEntity<ScheduledLabExaminationDto> createScheduledExamination(@RequestParam("lbp")String lbp, @RequestParam("date")Date scheduledDate,@RequestParam("note")String note,HttpServletRequest request) {
-        return new ResponseEntity<>(labExaminationsService.createScheduledExamination(lbp, scheduledDate, note,request.getHeader("Authorization")),HttpStatus.OK);
+    public ResponseEntity<ScheduledLabExaminationDto> createScheduledExamination(@RequestParam("lbp")String lbp, @RequestParam("date")Long scheduledDate,@RequestParam("note")String note,HttpServletRequest request) {
+        return new ResponseEntity<>(labExaminationsService.createScheduledExamination(lbp, new Date(scheduledDate), note,request.getHeader("Authorization")),HttpStatus.OK);
     }
 
     @PutMapping("/update-status")
@@ -54,14 +54,14 @@ public class LaboratoryExaminationsController {
 
     @GetMapping("/count-scheduled_examinations/by-day")
     @PreAuthorize("hasAnyRole('ROLE_LAB_TEHNICAR','ROLE_VISI_LAB_TEHNICAR')")
-    public ResponseEntity<Integer> countScheduledExaminationsByDay(@RequestParam("date") Date date,HttpServletRequest request) {
-        return new ResponseEntity<>(labExaminationsService.listScheduledExaminationsByDay(date,request.getHeader("Authorization")).size(),HttpStatus.OK);
+    public ResponseEntity<Integer> countScheduledExaminationsByDay(@RequestParam("date") Long date,HttpServletRequest request) {
+        return new ResponseEntity<>(labExaminationsService.listScheduledExaminationsByDay(new Date(date),request.getHeader("Authorization")).size(),HttpStatus.OK);
     }
 
     @GetMapping("/list-scheduled_examinations/by-day")
     @PreAuthorize("hasAnyRole('ROLE_LAB_TEHNICAR','ROLE_VISI_LAB_TEHNICAR')")
-    public ResponseEntity<List<ScheduledLabExaminationDto>> listScheduledExaminationsByDay(@RequestParam("date") Date date,HttpServletRequest request) {
-        return new ResponseEntity<>(labExaminationsService.listScheduledExaminationsByDay(date,request.getHeader("Authorization")),HttpStatus.OK);
+    public ResponseEntity<List<ScheduledLabExaminationDto>> listScheduledExaminationsByDay(@RequestParam("date") Long date,HttpServletRequest request) {
+        return new ResponseEntity<>(labExaminationsService.listScheduledExaminationsByDay(new Date(date),request.getHeader("Authorization")),HttpStatus.OK);
     }
 
     @GetMapping("/list-scheduled-examinations")
@@ -74,14 +74,14 @@ public class LaboratoryExaminationsController {
     @GetMapping("/list-scheduled-examinations/by-lbp-date")
     @PreAuthorize("hasAnyRole('ROLE_LAB_TEHNICAR','ROLE_VISI_LAB_TEHNICAR')")
     public ResponseEntity<?> listScheduledExaminationsByLbpAndDate(@RequestParam("lbp")String lbp,
-                                                                    @RequestParam(required = false)Date startDate,
-                                                                    @RequestParam(required = false)Date endDate,
+                                                                    @RequestParam(required = false)Long startDate,
+                                                                    @RequestParam(required = false)Long endDate,
                                                                     @RequestParam(defaultValue = "0")Integer page,
                                                                     @RequestParam(defaultValue = "10")Integer size,
                                                                     HttpServletRequest request) {
         return new ResponseEntity<>(
-                labExaminationsService.listScheduledExaminationsByLbpAndDate(lbp,startDate,
-                        endDate,request.getHeader("Authorization"),page,size),
+                labExaminationsService.listScheduledExaminationsByLbpAndDate(lbp,new Date(startDate),
+                        new Date(endDate),request.getHeader("Authorization"),page,size),
                 HttpStatus.OK);
     }
 
