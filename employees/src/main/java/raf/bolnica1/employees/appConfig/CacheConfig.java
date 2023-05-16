7b46c8838ca03a399a06ed4db8306adceccb4096
fcolic <fcolic8920rn@raf.rs>
2023-05-16 08:19:34 +0200

    izmene

--- employees/src/main/java/raf/bolnica1/employees/appConfig/CacheConfig.java
+++ employees/src/main/java/raf/bolnica1/employees/appConfig/CacheConfig.java
@@ -1,10 +1,20 @@
 package raf.bolnica1.employees.appConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String abc;
+
+    @Bean
+    public void test(){
+        System.out.println("app prop: " + abc);
+    }
 
 }
