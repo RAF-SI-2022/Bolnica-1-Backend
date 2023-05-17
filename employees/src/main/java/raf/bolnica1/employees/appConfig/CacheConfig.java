159318abe43b6f215031246e24e4f4570e660712
fcolic <fcolic8920rn@raf.rs>
2023-05-17 10:37:27 +0200

    Dodata konfiguracija za redis

---
+++ employees/src/main/java/raf/bolnica1/employees/appConfig/CacheConfig.java
@@ -0,0 +1,19 @@
+package raf.bolnica1.employees.appConfig;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> deliveryRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
