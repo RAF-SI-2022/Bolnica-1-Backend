de5b82f211dc4db3d1b84b638dcb9beef4f1bd2d
stefanbalaz2 <stefanbalaz2@gmail.com>
2023-07-19 18:22:58 +0200

    sklonjeni cache config

--- employees/src/main/java/raf/bolnica1/employees/appConfig/CacheConfig.java
+++ employees/src/main/java/raf/bolnica1/employees/appConfig/CacheConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+/*
 @Configuration
 @EnableCaching
 public class CacheConfig {
@@ -17,3 +18,4 @@ public class CacheConfig {
         return template;
     }
 }
+*/
